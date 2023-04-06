74d76e2ab04c6d6eefef0f6ab466b65f07c7d558
Shivesh <itachi101lelshibe@gmail.com>
2023-04-06 16:54:13 +0530

    Hiding_Bot_Token

--- Main.py
+++ Main.py
@@ -1,12 +1,16 @@
-
-BOT_TOKEN = "MTA5MzQyNjg5MjEyNTc2NTY5Mw.GyMHNe.syjkwR6Gq26VPDBCU15S4M-cZNcvIXuvB_bigU"
+import os
 import discord
 from discord.ext import commands
 import interactions
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = interactions.Client(
     token=BOT_TOKEN,
 )
+
 
 
 @bot.command(
@@ -16,11 +20,11 @@ description="Provides you the necessary information."
 async def info(ctx: interactions.CommandContext):
     n = ctx.guild.member_count
     try:
-        embed = interactions.Embed(color=interactions.Color.GREEN,title="Information",footer={"Creator":"By Shivesh"})
+        embed = interactions.Embed(color=interactions.Color.GREEN,title="Information")
         embed.add_field(name="Server",value="GameStorm Beta", inline=False)
         embed.add_field(name="Number of members",value=str(n),inline=False)
         embed.description= "Basic information about the GameStorm server."
-        embed.add_field(name="GitHub", value="https://github.com/monsterreaper")
+        embed.add_field(name="My GitHub", value="https://github.com/monsterreaper")
     
         await ctx.send(embeds=embed)
 
