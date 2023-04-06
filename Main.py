31d8c270747832fd335b51a1de97221dc7c958a1
Shivesh <itachi101lelshibe@gmail.com>
2023-04-06 14:40:20 +0530

    New module and others

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
 import discord
+from discord.ext import commands
+
+
+BOT_TOKEN = "MTA5MzQyNjg5MjEyNTc2NTY5Mw.GyMHNe.syjkwR6Gq26VPDBCU15S4M-cZNcvIXuvB_bigU"
+import interactions
+
+bot = interactions.Client(
+    token=BOT_TOKEN,
+)
+
+@bot.command()
+async def info(ctx: interactions.CommandContext):
+    """information about the server"""
+    await ctx.send("Gamestorm Beta \n Date created: Dec 21, 2022 \n By Shivesh") 
+    
+
+bot.start()
