edce7e9d44c12068d589138e300ea1592b440202
Shivesh <itachi101lelshibe@gmail.com>
2023-04-06 16:24:14 +0530

    Info function

--- Main.py
+++ Main.py
@@ -1,18 +1,32 @@
-import discord
-from discord.ext import commands
-
 
 BOT_TOKEN = "MTA5MzQyNjg5MjEyNTc2NTY5Mw.GyMHNe.syjkwR6Gq26VPDBCU15S4M-cZNcvIXuvB_bigU"
+import discord
+from discord.ext import commands
 import interactions
 
 bot = interactions.Client(
     token=BOT_TOKEN,
 )
 
-@bot.command()
+
+@bot.command(
+name="info",
+description="Provides you the necessary information."
+)
 async def info(ctx: interactions.CommandContext):
-    """information about the server"""
-    await ctx.send("Gamestorm Beta \n Date created: Dec 21, 2022 \n By Shivesh") 
+    n = ctx.guild.member_count
+    try:
+        embed = interactions.Embed(color=interactions.Color.GREEN,title="Information",footer={"Creator":"By Shivesh"})
+        embed.add_field(name="Server",value="GameStorm Beta", inline=False)
+        embed.add_field(name="Number of members",value=str(n),inline=False)
+        embed.description= "Basic information about the GameStorm server."
+        embed.add_field(name="GitHub", value="https://github.com/monsterreaper")
     
+        await ctx.send(embeds=embed)
+
+    except Exception as e:
+         print(e)
+
+
 
 bot.start()
