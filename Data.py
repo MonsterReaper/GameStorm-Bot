4214b75aa96fbb81573534749a095297610dc40b
Shivesh <itachi101lelshibe@gmail.com>
2023-04-07 15:19:38 +0530

    New_features

--- Data.py
+++ Data.py
@@ -1,1 +1,64 @@
-import seaborn
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import string 
+
+
+# Load the chat data from the Excel file
+df = pd.read_excel('merged_file.xlsx')
+
+# Group the messages by the sender and count the number of messages per sender
+msg_count = df.groupby('Name')['Message'].count()
+
+x = msg_count.pop("Truth or Dare")
+y = msg_count.pop("StudyLion")
+msg_count.pop("IAM ADHU")
+msg_count.pop("Carl-bot")
+msg_count.pop("Shivesh")
+import matplotlib.pyplot as plt
+
+# Create a list of colors for the pie chart
+colors = ['lightcoral', 'gold', 'lightskyblue', 'yellowgreen', 'pink', 'orange']
+
+# Create a figure and axis object
+fig, ax = plt.subplots()
+
+# Create a pie chart using the msg_count data
+ax.pie(msg_count.values, labels=msg_count.index, colors=colors, autopct='%1.1f%%', startangle=90)
+
+# Set the title of the pie chart
+ax.set_title('Message Count by Sender')
+
+# Show the plot
+plt.savefig('pie.png')
+
+
+# Print the number of messages per sender
+print(msg_count)
+
+# Create a dictionary to store the words and their frequency for each sender
+word_freq = {}
+
+# Loop through each sender's messages and tokenize the words
+for sender in df['Name'].unique():
+    # Combine all messages from the sender into a single string
+    sender_msgs = ' '.join(df.loc[df['Name'] == sender, 'Message'].fillna('').astype(str).tolist())
+    # Tokenize the words
+    words = sender_msgs.lower().translate(str.maketrans('', '', string.punctuation)).split()
+    # Calculate the frequency of each word and store it in the dictionary
+    word_freq[sender] = {}
+    for word in words:
+        if word in word_freq[sender]:
+            word_freq[sender][word] += 1
+        else:
+            word_freq[sender][word] = 1
+
+# Print the top 5 most used words for each sender
+word_freq_dict = {}
+for sender in word_freq:
+    word_freq_dict[sender] = {}
+    sorted_words = sorted(word_freq[sender].items(), key=lambda x: x[1], reverse=True)
+    for i in range(5):
+        if i < len(sorted_words):
+            word_freq_dict[sender][sorted_words[i][0]] = sorted_words[i][1]
+print(word_freq_dict)
--- Main.py
+++ Main.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 import interactions
 from dotenv import load_dotenv
 import datetime
+import wikipedia
+import base64
+from Data import msg_count, word_freq_dict
+import typing
 
 load_dotenv()
 
@@ -17,7 +21,6 @@ bot = interactions.Client(
 @bot.command(
 name="info",
 description="Provides you the necessary information.",
-
 )
 
 async def info(ctx: interactions.CommandContext):
@@ -81,6 +84,56 @@ async def reminder(ctx: interactions.CommandContext,hours: int, minutes: int, re
             not_reminded = False
     
     
+@bot.command(
+name="activity",
+description="Shows the activity of the server"
+)
+
+async def activity(ctx: interactions.CommandContext):
+    embed = interactions.Embed(color=interactions.Color.BLURPLE)
+    embed.description="Pie chart of activities"
+    x = "https://lh3.googleusercontent.com/pw/AMWts8As6-u7TGGFILLL2RwiIl4sNF_RaxnQzRI1euKsG789CJHPZbcOUqB18xlsb6ExLdoatiokQIoCOcckA5fXu0M8IJgLbgPDp8dM6GfQx6IimXlAwnMiqMZPpgLRaVfnAiGgIAIYLSJHefNx7MEPYxqA3Jzn_waUFJdFyu5WF46G6WsLTROBANkTo-kjPVRl2p6REYkbWgGuJ7ArPUC_7X504LMIlf2yqNdbUFGacth316KWBkE18RIl6CYJbaxCQGgSkm7icTTZxuVhI01ATqC6jVK-_ILdUZ2dZi1TDLZkSx4yljDXH7gD0ZTzy1jRoXEBTTmQSDMgEL_7sW7CZpxlGnrU1_BtZe3-4T8D-pmv8pTlAoPvb1Seg_8bcf5OrVXnfjlS6rPqGDFojWypKi4YOr-4_iPJzBpwfsTzh2cKcsZkxNgzD9azp4OF5xfQXpV2r0O9kZ_1ctP86lJT1PDJrwuwZAudk1oL8zgJ1Wvn6DaMC-2bRORZaXj6zBCa1RSL81Ntiow3UE4IN88-zuvhkLRWL8qkEVFlzGZYGW9GWoaoDE-vEOskLsBaFkDnAIitVKIqn-w6krDeFp8yFji4KW6Qjalq9DGFy2mkEIDAvH4WD7o8TuQJ6bDqPt_2lT8adiv82CzIFRPHWnz-TzHdg3oexUK5pzoyGqjsSlENTPgmFa6R0D2kwIhisPLYjuziZV-YVnIaq3JsDRl8kauUSaap-5Djux0i7FlOlUNUSD3cYFqFKdHHZJZXUv51MjTcZlmR71izfihCzD2nefXwnm8wRbGwJLD2aGOMdFfq4UWdHYfVWR-TE_TZdOSRj5IAoy6VPRrM3RFaz_EfUJ6sIRbqa_dk6VAfMMC9xmrqGvrhFFp9zxOhifo6pcIAhROTiKjrjGLBjH94pBO7Z2zc=w640-h480-s-no?authuser=0"
+    embed.set_image(x)
+    embed.footer = "By Shivesh"
+    embed.add_field(name="My GitHub", value="https://github.com/monsterreaper")
+    await ctx.send(embeds=embed)
+    
+
+@bot.command(
+name="my_activity",
+description="Shows the activity of the server"
+)
+
+async def my_activity(ctx: interactions.CommandContext):
+    user = ctx.user.username
+    embed = interactions.Embed(title="Your Activities",color=interactions.Color.BLURPLE)
+    embed.description="Your activities"
+    embed.add_field(name="Message Count", value=f"{msg_count[user]} messages")
+    embed.add_field(name="Top 5 words", value=str(word_freq_dict[user]).replace("{","").replace("}","").replace("'",""))
+    x = ctx.user.avatar_url
+    embed.set_image(x)
+    embed.footer = "By Shivesh"
+    embed.add_field(name="My GitHub", value="https://github.com/monsterreaper")
+    await ctx.send(embeds=embed)
+
+@bot.command(
+    name="avatar",
+    description="Shows the user's avatar",
+)
+async def avatar(ctx: interactions.CommandContext,user: typing.Optional[discord.Member]=None):
+    embed = interactions.Embed(title="Avatar",color=interactions.Color.FUCHSIA)
+    
+    user = ctx.user
+    user = print(bot.guilds[0].get_member(user))
+    embed.description=f"{user.username}'s avatar:"
+    
+    
+    embed.set_image(user.avatar_url)
+    
+    embed.footer = "By Shivesh"
+    await ctx.send(embeds=embed)
+
+
 
 
 
